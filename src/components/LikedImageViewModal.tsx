c58de4bd190ba2e7c8e4c1db6ab3f3f3ec6b7b0b
Kels-69 <johnquilly@optinetsolutions.com>
2026-05-15 20:36:29 +0800

    auto: Claude edit

--- src/components/LikedImageViewModal.tsx
+++ src/components/LikedImageViewModal.tsx
@@ -129,7 +129,7 @@ export function LikedImageViewModal({ isOpen, onClose, imgUrl, recordId }: Liked
                 <ChevronDown className="w-4 h-4 opacity-80" />
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
+            <DropdownMenuContent align="end" className="w-56 z-[1200]">
               <DropdownMenuItem onClick={handleDownload} className="gap-2">
                 <Download className="w-4 h-4" /> Normal
               </DropdownMenuItem>

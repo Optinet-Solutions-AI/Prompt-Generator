59b08078e529fce6eaf58281ae0175c70f1abbbd
Kels-69 <johnquilly@optinetsolutions.com>
2026-05-15 22:17:19 +0800

    auto: Claude edit

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/library" element={<ImageLibrary />} />
+          <Route path="/assistant/:token" element={<AssistantPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
